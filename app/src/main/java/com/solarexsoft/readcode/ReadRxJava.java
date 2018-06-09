c11c985befae70f23dd770257cf3582a727c531a
Solarex <rh.hou.work@gmail.com>
2018-06-09 09:52:09 +0800

    bind

--- app/src/main/java/com/solarexsoft/readcode/ReadRxJava.java
+++ app/src/main/java/com/solarexsoft/readcode/ReadRxJava.java
@@ -1,6 +1,16 @@
 package com.solarexsoft.readcode;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 public class ReadRxJava extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rxjava);
+        ButterKnife.bind(this);
+    }
 }
--- app/src/main/java/com/solarexsoft/readcode/ReadVolley.java
+++ app/src/main/java/com/solarexsoft/readcode/ReadVolley.java
@@ -1,6 +1,16 @@
 package com.solarexsoft.readcode;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 public class ReadVolley extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.volley);
+        ButterKnife.bind(this);
+    }
 }
