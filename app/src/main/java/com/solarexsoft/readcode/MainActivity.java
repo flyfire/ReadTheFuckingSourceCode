82d2fb7bb4a9b896202f1b9c111a73f00b64f6c6
Solarex <rh.hou.work@gmail.com>
2018-06-09 17:57:47 +0800

    http logging

--- app/src/main/java/com/solarexsoft/readcode/MainActivity.java
+++ app/src/main/java/com/solarexsoft/readcode/MainActivity.java
@@ -1,9 +1,12 @@
 package com.solarexsoft.readcode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.okhttp, R.id.retrofit, R.id.volley, R.id.glide, R.id.rxjava})
+    public void click(View view) {
+        int id = view.getId();
+        Intent intent = null;
+        if (id == R.id.okhttp) {
+            intent = new Intent(this, ReadOkhttp.class);
+        } else if (id == R.id.retrofit) {
+            intent = new Intent(this, ReadRetrofit.class);
+        } else if (id == R.id.volley) {
+            intent = new Intent(this, ReadVolley.class);
+        } else if (id == R.id.glide) {
+            intent = new Intent(this, ReadGlide.class);
+        } else if (id == R.id.rxjava) {
+            intent = new Intent(this, ReadRxJava.class);
+        }
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/solarexsoft/readcode/ReadOkhttp.java
+++ app/src/main/java/com/solarexsoft/readcode/ReadOkhttp.java
@@ -3,13 +3,23 @@ package com.solarexsoft.readcode;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ReadOkhttp extends AppCompatActivity {
+    public static final String TAG = ReadOkhttp.class.getSimpleName();
     OkHttpClient mOkHttpClient;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,7 +30,11 @@ public class ReadOkhttp extends AppCompatActivity {
     }
 
     private void init() {
-        mOkHttpClient = new OkHttpClient();
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        mOkHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
     }
 
     @OnClick({R.id.syncget, R.id.asyncget, R.id.interceptor})
@@ -36,9 +50,38 @@ public class ReadOkhttp extends AppCompatActivity {
     }
 
     private void syncget() {
+        Executors.newFixedThreadPool(1).submit(new Runnable() {
+            @Override
+            public void run() {
+                Request request = new Request.Builder().url("http://www.baidu.com").get().build();
+                Call call = mOkHttpClient.newCall(request);
+                try {
+                    Response response = call.execute();
+                    String body = response.body().string();
+                    Log.d(TAG, body);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void asyncget() {
+        Request request = new Request.Builder().url("http://www.baidu.com").get().build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String body = response.body().string();
+                Log.d(TAG, body);
+            }
+        });
     }
 
     private void interceptor() {
